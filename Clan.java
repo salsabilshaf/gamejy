da84d9ad19780147e7d09c41931bbaa2d0b57b33
Fahad Hafizudin Dayana <fahadhafiz5@gmail.com>
2017-05-20 21:03:04 +0700

    Update Clan.java

--- Clan.java
+++ Clan.java
@@ -3,3 +3,20 @@ package application;
 public class Clan {
     private String clan;
     private Integer jmhMbr,th,lv,pr;
+    public Clan(){
+		this.clan = "";
+		this.jmhMbr = 0;
+		this.th = 0;
+		this.lv = 0;
+		this.pr = 0;
+	}
+	public Clan(String clan,int jmhMbr, int th, int lv, int pr){
+		this.clan = clan;
+		this.jmhMbr = jmhMbr;
+		this.th = th;
+		this.lv = lv;
+		this.pr = pr;
+	}
+	public String getClan() {
+        return clan;
+    }
